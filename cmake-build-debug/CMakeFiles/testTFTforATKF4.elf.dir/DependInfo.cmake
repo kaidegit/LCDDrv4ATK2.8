4dd7b4bdfe48e6e684b526d80c37a5bb037e7b17
kai <2857693944@qq.com>
2021-06-06 20:50:12 +0800

    init

--- cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/DependInfo.cmake
@@ -6,7 +6,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_ASM
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/startup/startup_stm32f407xx.s" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/startup/startup_stm32f407xx.s.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/startup/startup_stm32f407xx.s" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/startup/startup_stm32f407xx.s.obj"
   )
 set(CMAKE_ASM_COMPILER_ID "GNU")
 
@@ -26,38 +26,38 @@ set(CMAKE_ASM_TARGET_INCLUDE_PATH
   "../Hardwares/LCD"
   )
 set(CMAKE_DEPENDS_CHECK_C
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Core/Src/fsmc.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/fsmc.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Core/Src/gpio.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/gpio.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Core/Src/i2c.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/i2c.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Core/Src/main.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/main.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Core/Src/pic.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/pic.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Core/Src/stm32f4xx_hal_msp.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Core/Src/stm32f4xx_it.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/stm32f4xx_it.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Core/Src/syscalls.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/syscalls.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Core/Src/system_stm32f4xx.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/system_stm32f4xx.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Core/Src/usart.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/usart.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_sram.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_sram.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_fsmc.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_fsmc.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Hardwares/LCD/lcd.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Hardwares/LCD/lcd.c.obj"
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Hardwares/touch/touch.c" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Hardwares/touch/touch.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Core/Src/fsmc.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/fsmc.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Core/Src/gpio.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/gpio.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Core/Src/i2c.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/i2c.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Core/Src/main.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/main.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Core/Src/stm32f4xx_hal_msp.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/stm32f4xx_hal_msp.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Core/Src/stm32f4xx_it.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/stm32f4xx_it.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Core/Src/syscalls.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/syscalls.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Core/Src/system_stm32f4xx.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/system_stm32f4xx.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Core/Src/usart.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/usart.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_i2c_ex.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_sram.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_sram.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_fsmc.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_fsmc.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Hardwares/LCD/lcd.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Hardwares/LCD/lcd.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Hardwares/LCD/pic.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Hardwares/LCD/pic.c.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Hardwares/touch/touch.c" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Hardwares/touch/touch.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
@@ -77,7 +77,7 @@ set(CMAKE_C_TARGET_INCLUDE_PATH
   "../Hardwares/LCD"
   )
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/Core/Src/main_cpp.cpp" "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/main_cpp.cpp.obj"
+  "/Users/kai/CLionProjects/LCDDrv4ATK2.8/Core/Src/main_cpp.cpp" "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/CMakeFiles/testTFTforATKF4.elf.dir/Core/Src/main_cpp.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341
+# Install script for directory: /Users/kai/CLionProjects/LCDDrv4ATK2.8
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -32,6 +32,11 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "TRUE")
 endif()
 
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "arm-none-eabi-objdump")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -40,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/kai/Desktop/mySync/mySTProject/testTFTforATKF4_9341/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/kai/CLionProjects/LCDDrv4ATK2.8/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
